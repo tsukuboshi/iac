bcb8c9cd2cd7cb501d16c8f26ec114636abc3770
Kaito Yoshiba <silk.120.k6god3@gmail.com>
2022-03-17 16:38:09 +0900

    Change order

--- Terraform/minimum_configuration/src/create_access_key.sh
+++ Terraform/minimum_configuration/src/create_access_key.sh
@@ -2,7 +2,7 @@
 # Run this shell in "terraform init" directory before executing "terraform apply".
 
 if [ -f ~/.ssh/minimum_configuration.pem ]; then
-  ssh-keygen -t rsa -f ~/.ssh/minimum_configuration.pem
-else
   echo "You have already created access key."
+else
+  ssh-keygen -t rsa -f ~/.ssh/minimum_configuration.pem
 fi
