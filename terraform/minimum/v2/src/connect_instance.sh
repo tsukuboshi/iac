b95438d89dbb3436f8f6cc46d7da181d3d3da208
Kaito Yoshiba <silk.120.k6god3@gmail.com>
2022-03-25 22:21:15 +0900

    Change connect instance script

--- terraform/minimum/v2/src/connect_instance.sh
+++ terraform/minimum/v2/src/connect_instance.sh
@@ -6,5 +6,5 @@ INSTANCE_ID="$(terraform output -json | jq -r .instance_id.value)"
 if [ ! "${INSTANCE_ID}" = null ]; then
   ssh -i ~/.ssh/minimum_tf.pem ec2-user@"${INSTANCE_ID}"
 else
-  echo "Instance public ip is null."
+  echo "Instance id is null."
 fi
