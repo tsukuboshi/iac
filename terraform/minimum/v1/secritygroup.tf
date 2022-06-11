ade0fe1544188f7ae0de35c9a30661fc957099e6
tsukuboshi <silk.120.k6god3@gmail.com>
2022-06-11 16:40:58 +0900

    Comment out ssh rule

--- terraform/minimum/v1/secritygroup.tf
+++ terraform/minimum/v1/secritygroup.tf
@@ -13,14 +13,14 @@ resource "aws_security_group" "tf_sg" {
 }
 
 # インバウンドルール(ssh接続用)
-resource "aws_security_group_rule" "in_ssh" {
-  security_group_id = aws_security_group.tf_sg.id
-  type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-}
+# resource "aws_security_group_rule" "in_ssh" {
+#   security_group_id = aws_security_group.tf_sg.id
+#   type              = "ingress"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+# }
 
 # アウトバウンドルール(全開放)
 resource "aws_security_group_rule" "out_all" {
